cfcc660b5a4abdf4707e93a655aa33f4cb759c1b
kimi <keddy406@hotmail.com>
2020-07-20 11:31:36 +0800

    error page update

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -2,7 +2,20 @@ import React from 'react';
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 const Error = () => {
-  return <h2>error page</h2>;
+  return (
+    // styled-components <Wrapper> 
+    <Wrapper>
+    <div>
+      <h1>404</h1>
+      <h3>sorry, the page you tried cannot be found</h3>
+      {/* className => index.css */}
+      <Link to="/" className="btn">
+        back home
+      </Link>
+    </div>
+  </Wrapper>
+ )
+  
 };
 const Wrapper = styled.section`
   min-height: 100vh;
